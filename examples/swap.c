a555e6878f507026179334a315b76af4cc099f88
Mateus Alves <mateus.alves.unb@gmail.com>
2021-04-23 14:14:24 -0300

    Learning about cpp function overloading

---
+++ examples/swap.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void swap_double(double *i, double *j)
+{
+    double temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+int main(int argc, char **argv)
+{
+    int m = 5, n = 10;
+    double x = 5.3, y = 10.6;
+
+    printf("inputs: %d, %d\n", m, n);
+    swap(&m, &n);
+    printf("outputs: %d, %d\n", m, n);
+
+    printf("double inputs: %lf, %lf\n", x, y);
+    swap_double(&x, &y);
+    printf("double outputs: %lf, %lf\n", x, y);
+}
+
